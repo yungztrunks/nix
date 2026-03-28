b7bd1d8113b444a6cd83c06f0ca9fa462eb3d4b2
weshford <95880628+weshford@users.noreply.github.com>
2026-03-28 13:22:14 +0100

    added new cli tools

--- home/weshy/packages/cli.nix
+++ home/weshy/packages/cli.nix
@@ -10,5 +10,16 @@ in
     programs.htop.enable = true;
     programs.fastfetch.enable = true;
     programs.weathr.enable = true;
+    programs.starship.enable = true;
+    programs.fzf.enable = true;
+    programs.bat.enable = true;
+    programs.neovim.enable = true;
   };
+
+  home.packages = with pkgs; [
+    fd
+    ripgrep
+    exa
+    btop
+  ];
 }
