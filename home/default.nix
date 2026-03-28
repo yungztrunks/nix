eb84c5e29a40449a7b0b0f1706c97af0e8199082
weshford <95880628+weshford@users.noreply.github.com>
2026-03-28 11:29:14 +0100

    backup ting yea

--- home/default.nix
+++ home/default.nix
@@ -3,5 +3,6 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "hm-backup";
   home-manager.users.${userName} = import ./${hostName}/default.nix;
 }
