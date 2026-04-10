bf9511a021c70bf1bfceb52fd5c7780b89090d96
yungztrunks <95880628+yungztrunks@users.noreply.github.com>
2026-04-10 23:15:47 +0200

    aspire specific

--- hosts/aspire/default.nix
+++ hosts/aspire/default.nix
@@ -1,5 +1,24 @@
 { ... }:
 
 {
-  # Aspire host module skeleton.
+  # Host-specific desktop selection controls.
+  my.modules.desktopSpecialisations = {
+    buildHyprland = true;
+    buildNiri = true;
+    buildKde = true;
+  };
+
+  # Host-specific multimedia stack.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  hardware.bluetooth.enable = true;
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true;
 }
