b2b65f7cc411f2b34858b595f2ca8a29391d54d1
weshford <95880628+weshford@users.noreply.github.com>
2026-04-10 23:42:57 +0200

    flake .

--- flake.nix
+++ flake.nix
@@ -19,30 +19,51 @@
 
   outputs = { nixpkgs, home-manager, noctalia, ... }:
     let
+      users = {
+        weshy = {
+          fullName = "weshy";
+          gitName = "weshford";
+          gitEmail = "95880628+weshford@users.noreply.github.com";
+          homeModule = ./users/weshy/default.nix;
+          extraGroups = [ "networkmanager" "wheel" ];
+        };
+      };
+
       hosts = {
         aspire = {
           system = "x86_64-linux";
-          users = {
-            weshy = {
-              fullName = "weshy";
-              gitName = "weshford";
-              gitEmail = "95880628+weshford@users.noreply.github.com";
-              homeModule = ./users/weshy/default.nix;
-              extraGroups = [ "networkmanager" "wheel" ];
-            };
-          };
+          userRefs = [ "weshy" ];
         };
       };
 
       enabledHosts = nixpkgs.lib.filterAttrs (_: hostCfg: hostCfg.enabled or true) hosts;
 
+      resolveUser = userName:
+        if builtins.hasAttr userName users then
+          (users.${userName} // {
+            username = users.${userName}.username or userName;
+          })
+        else
+          throw "Host references unknown user '${userName}'";
+
+      resolveHostUsers = hostCfg:
+        builtins.listToAttrs (map
+          (userName: {
+            name = userName;
+            value = resolveUser userName;
+          })
+          hostCfg.userRefs);
+
       mkHost = hostName: hostCfg:
+        let
+          hostUsers = resolveHostUsers hostCfg;
+        in
         nixpkgs.lib.nixosSystem {
           system = hostCfg.system;
           specialArgs = {
             inherit hostName;
             hostPath = ./hosts/${hostName};
-            hostUsers = hostCfg.users;
+            inherit hostUsers;
           };
           modules = [
             ./configuration.nix
@@ -53,12 +74,33 @@
               ];
             }
             (import ./users {
-              users = hostCfg.users;
+              users = hostUsers;
             })
           ];
         };
     in
     {
+      devShells = {
+        x86_64-linux.default =
+          let
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          in
+          pkgs.mkShell {
+            packages = with pkgs; [
+              git
+              just
+              nixfmt-rfc-style
+              statix
+              deadnix
+              nil
+              nixd
+              python3
+              uv
+              neovim
+            ];
+          };
+      };
+
       nixosConfigurations = nixpkgs.lib.mapAttrs mkHost enabledHosts;
     };
 }
