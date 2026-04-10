83a75f880fc58cafa218852244b008dd6bd3739f
weshford <95880628+weshford@users.noreply.github.com>
2026-04-10 23:15:47 +0200

    aspire specific

--- configuration.nix
+++ configuration.nix
@@ -31,9 +31,6 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
@@ -52,16 +49,6 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Enable Wayland support
-  security.pam.services.hyprlock = {};
-
-  # Enable the Hyprland Desktop Environment
-  services.displayManager.ly.enable = true;
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "de";
@@ -73,22 +60,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -108,15 +79,16 @@
     })
     hostUsers;
 
+  environment.pathsToLink = [
+    "/share/applications"
+    "/share/xdg-desktop-portal"
+  ];
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # added primarily for noctalia (to have working features)
-  hardware.bluetooth.enable = true;
-  services.upower.enable = true;
-  services.power-profiles-daemon.enable = true;
-
   my.modules.develop.enable = true;
+  my.modules.desktopSpecialisations.enable = true;
   my.modules.gaming.enable = true;
   my.modules.windowsApps.enable = true;
   my.modules.shellAliases.enable = true;
@@ -127,10 +99,7 @@
       # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wayland-utils
       wl-clipboard
-      hyprland
-      hyprlock
-      hypridle
-        kitty
+      kitty
       kdePackages.dolphin
       #  wget
   ];
