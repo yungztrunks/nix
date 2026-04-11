293ddb927a726d4574135f585b512fd1a60f5a63
yungztrunks <95880628+yungztrunks@users.noreply.github.com>
2026-04-11 13:32:41 +0200

    so isch besser

--- users/weshy/packages/apps.nix
+++ users/weshy/packages/apps.nix
@@ -1,22 +1,15 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
-let
-  cfg = config.my.home.apps;
-in
 {
-  options.my.home.apps.enable = lib.mkEnableOption "daily-use applications";
+  programs.kitty.enable = true;
+  programs.firefox.enable = true;
+  programs.obs-studio.enable = true;
 
-  config = lib.mkIf cfg.enable {
-    programs.kitty.enable = true;
-    programs.firefox.enable = true;
-    programs.obs-studio.enable = true;
-
-    home.packages = with pkgs; [
-      spotify
-      vlc
-      discord
-      puddletag
-      qbittorrent
-    ];
-  };
+  home.packages = with pkgs; [
+    spotify
+    vlc
+    discord
+    puddletag
+    qbittorrent
+  ];
 }
--- users/weshy/packages/cli.nix
+++ users/weshy/packages/cli.nix
@@ -1,20 +1,13 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
-let
-  cfg = config.my.home.cli;
-in
 {
-  options.my.home.cli.enable = lib.mkEnableOption "CLI tools and utilities";
-
-  config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      htop
-      fd
-      ripgrep
-      eza
-      btop
-      yazi
-      # TODO: sowas wie 7zip
-    ];
-  };
+  home.packages = with pkgs; [
+    htop
+    fd
+    ripgrep
+    eza
+    btop
+    yazi
+    # TODO: sowas wie 7zip
+  ];
 }
